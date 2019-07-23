cdaf36281fb89702f26fdf3e210ead69cfd51f84
gingikid <bsumser@gmail.com>
2019-07-22 19:32:33 -0700

    seperated parser into its own class

--- app/src/main/java/com/murach/winr/MainActivity.java
+++ app/src/main/java/com/murach/winr/MainActivity.java
@@ -1,48 +1,27 @@
 package com.murach.winr;
 
-import android.content.ContentValues;
 import android.content.Intent;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import android.view.View;
-
 import androidx.appcompat.app.AlertDialog;
 import androidx.core.view.GravityCompat;
 import androidx.appcompat.app.ActionBarDrawerToggle;
-
 import android.view.MenuItem;
-
 import com.google.android.material.navigation.NavigationView;
-
 import androidx.drawerlayout.widget.DrawerLayout;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.Menu;
-import android.widget.ListView;
 import android.widget.TextView;
 
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
-import org.xmlpull.v1.XmlPullParserFactory;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     private WineAdapter mAdapter;
     private SQLiteDatabase mDatabase;
+    private Parser mParser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +32,8 @@ public class MainActivity extends AppCompatActivity
         WineDBHelper dbHelper = new WineDBHelper(this);
         mDatabase = dbHelper.getWritableDatabase();
 
-        //parseXML();
+        //setup parser object
+        mParser = new Parser(this, mDatabase);
 
         /*-----------------------------BOILERPLATE CODE-------------------------------*/
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -62,7 +42,8 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                parseXML();
+                //parse the xml file
+                mParser.parseXML();
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -75,21 +56,6 @@ public class MainActivity extends AppCompatActivity
         /*-----------------------------END BOILERPLATE CODE-------------------------------*/
     }
 
-    private Cursor getAllItems() {
-        return mDatabase.query(
-                WineContract.WineEntry.TABLE_NAME,
-                null,
-                null,
-                null,
-                null,
-                null,
-                WineContract.WineEntry.COLUMN_PRICE + " DESC"
-        );
-    }
-
-    private Cursor getSomeItems() {
-        return mDatabase.rawQuery("SELECT * FROM " + WineContract.WineEntry.TABLE_NAME, null);
-    }
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -120,8 +86,7 @@ public class MainActivity extends AppCompatActivity
             Intent intent = new Intent(this, SettingsActivity.class);
             startActivity(intent);
             return true;
-        }
-        else if (id == R.id.menu_item_two) {
+        } else if (id == R.id.menu_item_two) {
             //launch the dialog fragment
             showAbout();
         }
@@ -135,6 +100,7 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
+        //launch different activities based on user click in navagation drawer
         if (id == R.id.red_wines) {
             Intent intent = new Intent(MainActivity.this, RedActivity.class);
             startActivity(intent);
@@ -148,14 +114,11 @@ public class MainActivity extends AppCompatActivity
             Intent intent = new Intent(MainActivity.this, RegionActivity.class);
             startActivity(intent);
         } else if (id == R.id.countries) {
-
         }
-
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-
 
     protected void showAbout() {
         // Inflate the about message contents
@@ -174,84 +137,4 @@ public class MainActivity extends AppCompatActivity
         builder.create();
         builder.show();
     }
-
-
-    private void parseXML() {
-        //declare instance of XmlPullParserFactory class
-        XmlPullParserFactory parserFactory;
-
-        //try/catch block to catch exceptions
-        try {
-            //instantiate instance of XmlPullParserFactory class
-            parserFactory = XmlPullParserFactory.newInstance();
-            XmlPullParser parser = parserFactory.newPullParser();
-
-            //get the input stream of xml file from the assests folder
-            InputStream is = getAssets().open("wineList.xml");
-            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
-            parser.setInput(is, null);
-
-            //take parser object and process into array of java objects
-            processParsing(parser);
-        } catch (XmlPullParserException e) {
-
-        } catch (IOException e) {
-        }
-    }
-
-    private void processParsing(XmlPullParser parser) throws IOException, XmlPullParserException {
-        int eventType = parser.getEventType();
-        //holder for values to be put in database
-        ContentValues cv = null;
-
-        while (eventType != XmlPullParser.END_DOCUMENT) {
-            //placeholder for tide data fields
-            String eltName = null;
-
-            switch (eventType) {
-                //switch case to determine which xml tag the loop is at
-                case XmlPullParser.START_TAG:
-                    eltName = parser.getName();
-
-                    //if else to determine which tag and add data to current java object
-                    if ("row".equals(eltName)) {
-                        cv = new ContentValues();
-                    } else if (cv != null) {
-                        if ("Color".equals(eltName)) {
-                            cv.put(WineContract.WineEntry.COLUMN_COLOR, parser.nextText());
-                        } else if ("Year".equals(eltName)) {
-                            cv.put(WineContract.WineEntry.COLUMN_YEAR, parser.nextText());
-                        } else if ("Varietal".equals(eltName)) {
-                            cv.put(WineContract.WineEntry.COLUMN_VARIETAL, parser.nextText());
-                        } else if ("Winery".equals(eltName)) {
-                            cv.put(WineContract.WineEntry.COLUMN_WINERY, parser.nextText());
-                        } else if ("Region".equals(eltName)) {
-                            cv.put(WineContract.WineEntry.COLUMN_REGION, parser.nextText());
-                        } else if ("Country".equals(eltName)) {
-                            cv.put(WineContract.WineEntry.COLUMN_COUNTRY, parser.nextText());
-                        } else if ("Price".equals(eltName)) {
-                            cv.put(WineContract.WineEntry.COLUMN_PRICE, parser.nextText());
-                        }
-                        mDatabase.insert(WineContract.WineEntry.TABLE_NAME, null, cv);
-                    }
-                    break;
-            }
-            eventType = parser.next();
-        }
-    }
-
-    /*//test function for printing data from xml
-    private void printDays(ArrayList<Day> days) {
-        StringBuilder builder = new StringBuilder();
-
-        for (Day day : days) {
-            builder.append(day.date).append("\n").
-                    append(day.day).append("\n").
-                    append(day.time).append("\n").
-                    append(day.ft).append("\n").
-                    append(day.cm).append("\n").
-                    append(day.highlow).append("\n\n");
-        }
-        //txt.setText(builder.toString());
-    }*/
 }
---
+++ app/src/main/java/com/murach/winr/Parser.java
@@ -0,0 +1,87 @@
+package com.murach.winr;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Parser {
+    private Context parseContext;
+    private SQLiteDatabase parseDatabase;
+
+    public Parser(Context context, SQLiteDatabase mDatabase) {
+        parseContext = context;
+        parseDatabase = mDatabase;
+    }
+
+    public void parseXML() {
+        //declare instance of XmlPullParserFactory class
+        XmlPullParserFactory parserFactory;
+
+        //try/catch block to catch exceptions
+        try {
+            //instantiate instance of XmlPullParserFactory class
+            parserFactory = XmlPullParserFactory.newInstance();
+            XmlPullParser parser = parserFactory.newPullParser();
+
+            //get the input stream of xml file from the assests folder
+            InputStream is = parseContext.getAssets().open("wineList.xml");
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.setInput(is, null);
+
+            //take parser object and process into array of java objects
+            processParsing(parser);
+        } catch (XmlPullParserException e) {
+
+        } catch (IOException e) {
+        }
+    }
+
+    public SQLiteDatabase processParsing(XmlPullParser parser) throws IOException, XmlPullParserException {
+        int eventType = parser.getEventType();
+        //holder for values to be put in database
+        ContentValues cv = null;
+
+        while (eventType != XmlPullParser.END_DOCUMENT) {
+            //placeholder for tide data fields
+            String eltName = null;
+
+            switch (eventType) {
+                //switch case to determine which xml tag the loop is at
+                case XmlPullParser.START_TAG:
+                    eltName = parser.getName();
+
+                    //if else to determine which tag and add data to current java object
+                    if ("row".equals(eltName)) {
+                        cv = new ContentValues();
+                    } else if (cv != null) {
+                        if ("Color".equals(eltName)) {
+                            cv.put(WineContract.WineEntry.COLUMN_COLOR, parser.nextText());
+                        } else if ("Year".equals(eltName)) {
+                            cv.put(WineContract.WineEntry.COLUMN_YEAR, parser.nextText());
+                        } else if ("Varietal".equals(eltName)) {
+                            cv.put(WineContract.WineEntry.COLUMN_VARIETAL, parser.nextText());
+                        } else if ("Winery".equals(eltName)) {
+                            cv.put(WineContract.WineEntry.COLUMN_WINERY, parser.nextText());
+                        } else if ("Region".equals(eltName)) {
+                            cv.put(WineContract.WineEntry.COLUMN_REGION, parser.nextText());
+                        } else if ("Country".equals(eltName)) {
+                            cv.put(WineContract.WineEntry.COLUMN_COUNTRY, parser.nextText());
+                        } else if ("Price".equals(eltName)) {
+                            cv.put(WineContract.WineEntry.COLUMN_PRICE, parser.nextText());
+                        }
+                    }
+                    parseDatabase.insert(WineContract.WineEntry.TABLE_NAME, null, cv);
+                    break;
+            }
+            eventType = parser.next();
+        }
+        return parseDatabase;
+    }
+}
